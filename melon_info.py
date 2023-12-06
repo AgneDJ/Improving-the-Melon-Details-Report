699c1342d2d1004507f3eea8a810aa1401e2127a
AgneDJ <fizikee@gmail.com>
2023-12-05 23:37:36 -0800

    New attributes added

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,19 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_prices, melon_seedlessness 
+from melons import melon_names, melon_prices, melon_seedlessness, melon_flesh_color, melon_rind_color, melon_average_weight
 
 
-def print_melon(name, price, seedless):
+def print_melon(name, price, seedless, flesh_color=None, rind_color=None, average_weight=None):
     """Print each melon with corresponding attribute information."""
 
     have_or_have_not = 'have'
     if seedless:
         have_or_have_not = 'do not have'
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price}')
+    print(f'{name}s flesh: {flesh_color} \n rind: {rind_color} \n weight {average_weight} \n {have_or_have_not} seeds and are ${price}')
 
 
 for i in melon_names:
-    print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
+    print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i],
+                melon_flesh_color[i], melon_rind_color[i], melon_average_weight[i])
